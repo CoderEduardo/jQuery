107c8f3287c7847fb513fd7a2893c2a4eb940b28
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-06-29 14:48:04 -0300

    Demonstrando como se funciona os encadeamentos com jQuery

---
+++ Encadeamento/script.js
@@ -0,0 +1,3 @@
+//Toda vez que usamos alguma função do jQuery, ele retorna ele mesmo, nos dando a possibilidade de executar várias coisas em um elemento
+let lista1 = $("#lista1")
+lista1.slideUp(2000).slideDown(1000).fadeOut(1000).fadeIn(1000)
