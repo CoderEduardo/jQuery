067560e3cf43fd42cb3af4dd0605584fa96c1a79
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-06-30 17:48:29 -0300

    Trabalhando com dimensões

---
+++ Dimensoes/script.js
@@ -0,0 +1,9 @@
+let caixa = $(".caixa")
+
+caixa.width() //Retorna o valor somente do witdth
+caixa.innerWidth() //Retorna o valor do width + padding
+caixa.outerWidh()   //Retorna o valor do width + padding + borda
+caixa.outerWidth(true)  //Retorna o valor do width + padding + borda + margin
+
+/*Podemos passar valores e modificar o tamanho original dos elementos, exemplo: caixa.width(2000)*/
+
